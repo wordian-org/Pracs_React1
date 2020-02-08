eb1adbf14352a7f967affbd743db967dffc55453
wordian-org <wordian.org@gmail.com>
2020-02-08 21:50:38 +0500

    2 files modified, 1 added

---
+++ Components/card.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Card extends Component{
+  render(){
+
+    return( 
+      <div>
+    <h1 class="">Card1</h1>
+    </div>);
+
+  }
+}
+
+
+export default Card;
--- index.js
+++ index.js
@@ -2,22 +2,18 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
+import Card from './Components/card.js';
+
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
+  
 
   render() {
     return (
       <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
+
+        <Card />
+        
       </div>
     );
   }
